cd927c32ed2b7cf09af9aa9a72a680553ecbe2e4
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-09-17 22:22:15 +0400

    adsense global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
+   <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6903318053588999"
+     crossOrigin="anonymous" />
    <Script src="https://www.googletagmanager.com/gtag/js?id=G-S653ZNY8SX" />
         <Script id="google-analytics">
         {`
