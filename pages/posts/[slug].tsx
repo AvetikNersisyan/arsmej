0d09d9c6bb8221bde966dc21fb9b40cf63b18cd7
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-09-18 17:28:38 +0400

    og:image fix

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -44,7 +44,7 @@ const currentUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${router.asPath}`;
 
                 <meta
                   property="og:image"
-                  content={post.featuredImage.node.sourceUrl}
+                  content={post.featuredImage?.node.sourceUrl}
                   // content={'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'}
                 />
                 <meta
